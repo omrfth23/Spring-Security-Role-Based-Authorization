018849bffe2212145612249d4ba6ee349375edee
omrfth23 <77546462+omrfth23@users.noreply.github.com>
2023-10-23 20:55:58 +0300

    Database authorization

--- src/main/java/com/omrfth/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/omrfth/cruddemo/security/DemoSecurityConfig.java
@@ -8,12 +8,38 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
+    UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employee").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employee/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employee").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employee/**").hasRole("ADMIN")
+
+        );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+      /*
     @Bean
     public InMemoryUserDetailsManager userDetailManager(){
 
@@ -36,22 +62,5 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(johnny,omer,daffy);
 
     }
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-
-        httpSecurity.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employee").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employee/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employee").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employee").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employee/**").hasRole("ADMIN")
-
-        );
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        httpSecurity.csrf(csrf -> csrf.disable());
-        return httpSecurity.build();
-    }
+*/
 }
